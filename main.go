61fd0990e3926db24450be3ac06b0d7c0650471d
darrenjon <darrenlin23@gmail.com>
2024-05-17 18:32:51 +0800

    feat: Add authentication with Google Sheets API

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
+
+var srv *sheets.Service
+
+// Initialize the Sheets service
+func initSheetsService() error {
+	b, err := os.ReadFile("service_account.json") // Use your service account JSON file
+	if err != nil {
+		return fmt.Errorf("unable to read service account file: %v", err)
+	}
+
+	conf, err := google.JWTConfigFromJSON(b, sheets.SpreadsheetsReadonlyScope)
+	if err != nil {
+		return fmt.Errorf("unable to parse service account file to config: %v", err)
+	}
+
+	client := conf.Client(context.Background())
+	srv, err = sheets.NewService(context.Background(), option.WithHTTPClient(client))
+	if err != nil {
+		return fmt.Errorf("unable to retrieve Sheets client: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	spreadsheetId := flag.String("id", "", "Google Sheet ID")
@@ -11,10 +41,13 @@ func main() {
 	flag.Parse()
 
 	if *spreadsheetId == "" {
-		fmt.Println("Please provide the Google Sheet ID")
-		return
+		log.Fatalf("Please provide the Google Sheet ID")
 	}
 
-	fmt.Printf("Spreadsheet ID: %s\n", *spreadsheetId)
-	fmt.Printf("Output File: %s\n", *outputFile)
+	// Initialize Sheets service
+	if err := initSheetsService(); err != nil {
+		log.Fatalf("Unable to initialize Sheets service: %v", err)
+	}
+
+	fmt.Printf("Google Sheets API initialized successfully. Output file: %s\n", *outputFile)
 }
